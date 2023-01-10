09e7603e37b148ac01d3980821c337e826cfcae0
amjan <amjan91@gmail.com>
2023-01-10 17:37:45 -0500

    base.html added

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,19 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
     user = {'username':'Amber'}
-    return render_template('index.html', title='Home', user=user)
+    posts = [
+        {
+            'author': {'username': 'Mike'},
+            'body': 'Beautiful day in Florida'
+        },
+        {
+            'author': {'username': 'Jane'},
+            'body': 'Went to the movies today!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
  
